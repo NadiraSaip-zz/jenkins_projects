46e3137f29990aa786614d187633f9d0b9cc7462
nsaip <nadiasaip@gmail.com>
2019-08-28 18:58:56 -0500

    UPDATED

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,13 @@
 node{
     stage("Update Jenkins"){
-          properties([parameters([string(defaultValue: '54.194.190.109', description: 'Please provide IP of the environment you want to deploy', name: 'Enviroment', trim: true)])])
+          properties([parameters([string(defaultValue: '54.194.190.109', description: 'Please provide IP of the environment you want to deploy', name: 'ENVIR', trim: true)])])
           sh "echo Parameter added"
     }
     stage("Install git"){
-        sh "ssh ec2_user@${Enviroment} sudo yum install git python-pip -y"
+        sh "ssh ec2_user@${ENVIR} sudo yum install git python-pip -y"
     }
     stage("Pull Repo"){
-        sh "ssh ec2_user@${Enviroment} git clone https://github.com/miguelgrinberg/flask-examples.git"
+        sh "ssh ec2_user@${ENVIR} git clone https://github.com/miguelgrinberg/flask-examples.git"
 
     }
     stage("Install Requirements"){
@@ -16,9 +16,9 @@ node{
         sh "echo Hello"
     }
     stage("Pip Install"){
-        sh "ssh ec2_user@${Enviroment} pip install -r ~/flask-examples/requirements.txt"
+        sh "ssh ec2_user@${ENVIR} pip install -r ~/flask-examples/requirements.txt"
     }
     stage("Run App"){
-        sh "ssh ec2_user@${Enviroment} python ~/flask-examples/01-hello-world/hello.py"
+        sh "ssh ec2_user@${ENVIR} python ~/flask-examples/01-hello-world/hello.py"
     }
 }
